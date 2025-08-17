5cf8e0db0e5c8f419ca46550c19ac51ee9d44a86
BensonTochukwu <tecotochukwu@gmail.com>
2025-08-17 13:13:21 +0100

    Background problem

--- weatherproject/weatherproject/settings.py
+++ weatherproject/weatherproject/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0n5g%lovif4=plzoul3eih@+2*ugnu+q@fs3@&u2!w5cxi1n4#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = ['weather-app-zpyc.onrender.com']
 
 
 # Application definition
