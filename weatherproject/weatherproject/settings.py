a06e3ba7b8287cd2aae57430f516ddc17f8b9cb0
BensonTochukwu <tecotochukwu@gmail.com>
2025-08-17 11:16:05 +0100

    Setup static files for Render

--- weatherproject/weatherproject/settings.py
+++ weatherproject/weatherproject/settings.py
@@ -131,3 +131,5 @@ GOOGLE_SEARCH_ENGINE_ID = os.getenv('GOOGLE_SEARCH_ENGINE_ID')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
